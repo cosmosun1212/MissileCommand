b18fa84a50db065f3c938e0e8200ba0526820a8a
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-04-15 16:22:12 +0900

    Add SetGameController method to BulletLauncher
    기존코드에서 contoller 검사를 통해 코드의 안정성 확보

--- BulletLauncher.cs
+++ BulletLauncher.cs
@@ -1,17 +1,32 @@
-public class BulletLauncher : MonoBehaviour
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine;
+public class BulletLauncher : MonoBehaviour 
 { 
   IGameController controller;
+  public void SetGameController(IGameController controller)
+  {
+      this.controller = controller;
+  }
+  
   private void Start()
   {
       controller = new MouseGameController();
   }
-
+  
   // Update is called once per frame
   void Update()
   {
+      if (controller != null)
+      {
           if (controller.FireButtonPressed())
           {
               Debug.Log("Fired a bullet!");
           }
+      }
+      else
+      {
+          Debug.Log("controller is null!");
+      }
   }
 }
