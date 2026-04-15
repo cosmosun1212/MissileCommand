bad0e5e905988a560b4ba377722199eb309c3e3b
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-04-15 16:08:54 +0900

    Add comment for IGameController interface in JoystickGameController

---
+++ JoystickGameController.cs
@@ -0,0 +1,11 @@
+//IGameContoller interface 조이스틱 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class JoystickGameController : IGameController
+{
+  public bool FireButtonPressed()
+  {
+    throw new System.NotImplementedException();
+  }
+}
