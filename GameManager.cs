5d22a1fe00c9df331896884cecdb771b71983f7e
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-04-16 11:10:00 +0900

    Refactor GameManager to support multiple controllers

--- GameManager.cs
+++ GameManager.cs
@@ -1,7 +1,7 @@
-//
 using System.Collections; 
 using System.Collections.Generic; 
 using UnityEngine;
+
 public class GameManager : MonoBehaviour 
 { 
     [SerializeField] 
@@ -10,10 +10,14 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update 
     void Start() 
     { 
-        launcher = Instantiate(launcherPrefab); 
-        launcher.SetGameController(new MouseGameController()); //Dependency Injection 코드
-        //launcher.SetGameController(new KeyGameController()); //BulletLauncher launcher 하나로 하나의 입력만
+        launcher = Instantiate(launcherPrefab);
+        MouseGameController mouseController = gameObject.AddComponent<MouseGameController>();
+        mouseController.FireButtonPressed += launcher.OnFireButtonPressed;
+    
+        KeyGameController keyController = gameObject.AddComponent<KeyGameController>();
+        keyController.FireButtonPressed += launcher.OnFireButtonPressed;
     }
+    
     // Update is called once per frame
     void Update()
     {
