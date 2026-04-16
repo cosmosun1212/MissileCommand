5c797016eb4cd0655f3ce8574d1665900a7775db
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-04-16 10:51:39 +0900

    Implement KeyGameController for firing actions

---
+++ KeyGameController.cs
@@ -0,0 +1,18 @@
+using System; 
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine;
+public class KeyGameController : MonoBehaviour, IGameController 
+{
+    public Action FireButtonPressed;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (FireButtonPressed != null)
+            {
+                FireButtonPressed();
+            }
+        }
+    }
+}
