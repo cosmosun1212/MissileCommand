9a99e738de75ab628634b55010cfab24d5b21cb7
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-04-16 10:53:32 +0900

    Update BulletLauncher.cs

--- BulletLauncher.cs
+++ BulletLauncher.cs
@@ -1,18 +1,20 @@
 //마우스입력, 키보드 입력 //강한 결합
-using System.Collections; using System.Collections.Generic; using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine;
 public class BulletLauncher : MonoBehaviour 
 { 
-  // Start is called before the first frame update 
-  void Start() 
-  {
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-      if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
-      { 
-          Debug.Log("총알 발사!!!");
-      }
-  }
+    // Start is called before the first frame update 
+    void Start() 
+    {
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+          if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+          { 
+              Debug.Log("총알 발사!!!");
+          }
+    }
 }
