6265da7e4cae2aec879879cfabdb7b0a0b953df4
Kim DaeJin <78810280+cosmosun1212@users.noreply.github.com>
2026-04-16 11:08:49 +0900

    Refactor BulletLauncher to remove dependency injection
    Removed dependency injection and simplified the BulletLauncher class.

--- BulletLauncher.cs
+++ BulletLauncher.cs
@@ -1,36 +1,16 @@
-//Dependency Injection  코드 추가
-
 using System.Collections; 
 using System.Collections.Generic; 
 using UnityEngine;
-public class BulletLauncher : MonoBehaviour 
-{ 
-  IGameController controller;
 
-  public void SetGameController(IGameController controller)
-  {
-      this.controller = controller;
-  }
-
-  //Dependecy Injection으로 전환할 경우 //Start()함수 삭제
-  private void Start()
-  {
-      controller = new MouseGameController();
-  }
-  
-  // Update is called once per frame
-  void Update()
-  {
-      if (controller != null)
-      {
-          if (controller.FireButtonPressed())
-          {
-              Debug.Log("Fired a bullet!");
-          }
-      }
-      else
-      {
-          Debug.Log("controller is null!");
-      }
-  }
+    public class BulletLauncher : MonoBehaviour 
+    { 
+        // Update is called once per frame 
+        void Update()
+    {
+    }
+    
+    public void OnFireButtonPressed()
+    {
+        Debug.Log("Fired a bullet!");
+    }
 }
